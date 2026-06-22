d7f0b3cb7f4a39a59f830d7fa0511f7b8d101521
lztto <jisung7460@gmail.com>
2026-06-22 11:05:06 +0900

    backend 내용 수정

--- backend/src/main/java/com/bank/dto/SignupRequest.java
+++ backend/src/main/java/com/bank/dto/SignupRequest.java
@@ -1,5 +1,21 @@
 package com.bank.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class SignupRequest {
 
+    @NotBlank(message = "아이디는 필수입니다.")
+    private String memberId;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;
+
+    @NotBlank(message = "전화번호는 필수입니다.")
+    private String phone;
 }
